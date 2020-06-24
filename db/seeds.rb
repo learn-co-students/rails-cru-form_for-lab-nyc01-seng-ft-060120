4f93e4cc3b64518eb351f07a69beb603d618a910
liam <leamsi.esc@gmail.com>
2020-06-24 10:41:16 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+drake = Artist.create({:name => "Drake",
+  :bio => "Born Aubrey Drake Graham on October 24, 1986, in Toronto,
+  Canada, Drake grew up with music in his blood. His father, Dennis Graham,
+  was a drummer for the legendary rock 'n' roll star Jerry Lee Lewis. ...
+  His father is an African American Catholic and his mother is a white Canadian Jew"})
+
+hiphop = Genre.create({:name => "Hip Hop"})
+
+
+Song.create({:name => "Hotline Bling", :artist_id => drake.id, :genre_id => hiphop.id})
+Song.create({:name => "Tootsie Roll", :artist_id => drake.id, :genre_id => hiphop.id})
+Song.create({:name => "God's Plan", :artist_id => drake.id, :genre_id => hiphop.id})
