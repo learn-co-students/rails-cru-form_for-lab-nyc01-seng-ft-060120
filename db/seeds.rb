0a344257248b928fb23b8754e1c585171b82c4a7
mayalexandera <maya.blevins@gmail.com>
2020-06-24 21:52:27 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do |artist|
+  Artist.create(
+    name: "Artist #{artist}",
+    bio: "Bio #{artist}"
+  )
+end
+
+5.times do |genre|
+  Genre.create(
+    name: "genre #{genre}",
+  )
+end
+
+5.times do |song|
+  Song.create(
+    name: "song #{song}",
+    artist_id: Artist.last.id,
+    genre_id: Genre.last.id
+  )
+end
